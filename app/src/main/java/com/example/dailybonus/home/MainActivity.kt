4cb058500ad3781c4407fd1b9d8d4ba60e4d5504
higuzero <higuzeroggw@gmail.com>
2020-08-17 22:44:56 +0900

    [solve] DataBindingUtil.setContentView(this, R.layout.activity_home)のようにしてViewの読み込みを行う
     res/layoutで定義したレイアウトファイルを元にViewを組み立てるには、何らかの方法でXMLを読み込んでやる必要がある

--- app/src/main/java/com/example/dailybonus/home/MainActivity.kt
+++ app/src/main/java/com/example/dailybonus/home/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.dailybonus.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.dailybonus.R
+import com.example.dailybonus.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     }
 }
